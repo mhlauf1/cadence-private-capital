9680cf6ef548f467c97aeb7bd649209b8dbbeb7e
mhlauf1 <mhlauf1@gmail.com>
2024-09-29 10:41:24 -0500

    rfooter fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col-reverse gap-y-6 md:flex-row items-center px-8 md:px-20 py-6">
+    <footer className="flex flex-col-reverse gap-y-6 md:flex-row items-center px-8 md:px-20 py-12 md:py-6">
       <div className="flex flex-row items-center  flex-grow flex-1  gap-1">
-        <FaRegCopyright color="grey" size={10} />
-        <p className="text-neutral-500 text-sm text-center md:text-left">
+        <p className="text-neutral-500 flex items-center  gap-1 text-sm text-center md:text-left">
+          <FaRegCopyright color="grey" size={10} />
           Cadence Private Capital 2024. All rights reserved.
         </p>
       </div>
@@ -21,6 +21,7 @@ const Footer = () => {
         <Image
           src="/cpc-footer-logo.png"
           alt="Footer logo"
+          className="w-[120px] h-[90px] md:w-[100px] md:h-[75px]"
           width={90}
           height={70}
         />
