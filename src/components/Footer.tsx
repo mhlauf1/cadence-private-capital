2cc328b29cbaf5726207ea1c68071441dc205aa2
mikelauf <michael.laufersweiler@lulafit.com>
2024-11-19 17:54:02 -0600

    added Lauf footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Image from "next/image";
-import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="flex flex-col-reverse gap-y-6 md:flex-row items-center px-8 md:px-20 py-12 md:py-6">
-      <div className="flex flex-row items-center  flex-grow flex-1  gap-1">
-        <p className="text-neutral-500  text-xs md:text-sm text-center md:text-left">
+      <div className="flex flex-col items-center md:items-start gap-1 flex-1">
+        <p className="text-neutral-500  text-xs  text-center md:text-left">
           &copy; Cadence Private Capital 2024. All rights reserved.
+        </p>
+        <p className="text-neutral-500  text-xs  text-center md:text-left">
+          Designed and developed by{" "}
+          <a href="www.lauf.co" target="_blank" className="font-medium">
+            Lauf.
+          </a>
         </p>
       </div>
 
-      <div className="flex-grow text-neutral-500 flex-1 text-xs md:text-sm text-center">
+      <div className="flex-grow text-neutral-500 flex-1 text-xs  text-center">
         <a href="mailto:info@cadenceprivatecapital.com">
           info@cadenceprivatecapital.com
         </a>
@@ -20,7 +25,7 @@ const Footer = () => {
         <Image
           src="/cpc-blue-logo.png"
           alt="Footer logo"
-          className="w-[120px] h-[90px] md:w-[100px] md:h-[75px]"
+          className="w-[120px] h-[90px] md:w-[90px] md:h-[70px]"
           width={90}
           height={70}
         />
